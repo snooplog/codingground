e4b449be21a87e6ca19d976ca1f11d802bc32511
snooplog <matiss.margerslol@gmail.com>
2016-12-14 13:55:13 +0200

    Created by Coding Ground Client

---
+++ New Project-20161214/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x;
+ int y;
+ 
+ x = 2016;
+ y = 13; 
+   
+   cout << "    _____________________________________" <<endl;
+   cout << "   |Apsveicu tevi tavos " << y << ". Ziemassvetkos|" <<endl;
+   cout << "   |Saja " << x << ". gada!                     |" <<endl;
+   cout << "   |  *                                  |" <<endl;
+   cout << "   | ***                                 |" <<endl;
+   cout << "   |*****                                |" <<endl;
+   cout << "   |  *                                  |" <<endl;
+   cout << "   |                        Matiss Barons|" <<endl;
+   cout << "   |_____________________________________|" <<endl;
+   return 0;
+}
