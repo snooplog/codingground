098ffd1d244eaa8e0f6dda1965da77d23894a4a5
snooplog <matiss.margerslol@gmail.com>
2017-01-18 13:32:32 +0200

    Created by Coding Ground Client

---
+++ New Project-20170118/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   int y;
+   int z;
+   int summa;
+   int starpiba;
+   int reizinajums;
+   int mod;
+   double dalijums;
+   /*
+   x, y, z
+   
+   2x + 3y + z
+   (xy + xz) * 2
+   x*2 * (y - z)
+   x = 110101;
+   y = 10001;
+   z = 101011;
+   
+   summa = x + y;
+   starpiba = x - y;
+   reizinajums = x * y;
+   dalijums = (double)x / (double)y;
+   
+   cout << "Skaitla " << x << " un " << y << " summa ir " << summa << endl;
+   cout << "Skaitla " << x << " un " << y << " starpiba ir " << starpiba << endl;
+   cout << "Skaitla " << x << " un " << y << " reizinajums ir " << reizinajums << endl;
+   cout << "Skaitla " << x << " un " << y << " dalijums ir " << dalijums << endl;
+   cout << "Skaitla " << x << " un " << y << " mod ir " << mod << endl;
+   cout <<
+   return 0;
+}
+
